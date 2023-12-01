e342c77f87cfc41bc211e8e0f8930c2c55549a69
rudenchiks <146050710+rudenchiks@users.noreply.github.com>
2023-12-01 21:04:40 +1000

    add classes

--- hum/hum.cpp
+++ hum/hum.cpp
@@ -2,6 +2,46 @@
 
 using namespace std;
 
+class Human {
+public:
+	string GetName()
+	{
+		return name;
+	};
+
+	void SetName(name)
+	{
+		this->name = name;
+	};
+
+private:
+	string name;
+}
+
+class Student {
+public:
+	int group;
+	void study()
+	{
+		cout << "Я хожу на пары" << endl;
+	}
+};
+
+class ExtramuralStudent {
+public:
+	void study() 
+	{
+		cout << "Я на заочном обучении" << endl;
+	}
+
+};
+
+class Teacher {
+public:
+	string subject;
+};
+
+
 int main() {
 
 	
